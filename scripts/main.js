341117da140692f63ff29b7e5e53586eeee1263d
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-03-17 09:42:21 -0500

    this is mostly working

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import { getEateries } from "./eateries/EateryProvider.js";
 import {sendDropDownToDom} from "./nav/dropDown.js";
 import { getParks } from "./parks/ParkProvider.js";
 import { getWeather } from "./weather/WeatherProvider.js";
+import {useDb} from "./save/getDataFromDom.js";
 
-
+useDb();
 sendDropDownToDom()
