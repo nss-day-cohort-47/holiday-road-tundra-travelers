cf2cbe729cad97556e31940966a55874315441a9
Carter Culkin <carter.culkin@gmail.com>
2021-03-17 14:09:22 -0500

    A little cleaner, with some pointed out errors fixed

--- scripts/nav/nav.js
+++ scripts/nav/nav.js
@@ -1,36 +1,34 @@
 const makeNav = () => {
-return `
-<img class="pageimage" src="/images/patagonia.jpg" alt="patagoniaImg"><img class="logo" id="NavButton" src="./images/Geologo.jpg" alt="Geologo">
-      
-      <div id="mySidenav" class="sidenav">
-          <a href="javascript:void(0)" id="closebutton" class="closebtn" onclick="closeNav()">&times;</a>
-          <a href="/index.html">Home</a>
-          <a href="/pages/createnew.html">Create New</a>
-          <a href="/pages/saved.html">Saved</a>
-          <a href="/pages/search.html">Search?</a>
-          <a href="https://www.geocaching.com/play" target="_blank" rel="noopener noreferrer">Start Geocaching!</a>
-        </div>`
-}
-export const SendNavToDom = () => {
-    const navDom = document.querySelector(".Nav");
-    navDom.innerHTML += makeNav()
-}
-
-
-export function openNav(){
-    document.getElementById("NavButton").addEventListener("click", navOpen);
-}
-
-export function navOpen() {
-  document.getElementById("mySidenav").style.width = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-   
-
-export function closeNav() {
-    document.getElementById("closebutton").addEventListener("click", navClose)
-}
-export function navClose() {
-    document.getElementById("mySidenav").style.width = "0px";
-}
-
+    return `
+    <img class="pageimage" src="../images/patagonia.jpg" alt="patagoniaImg"><img class="logo" id="NavButton" src="../images/Geologo.jpg" alt="Geologo">
+          <div id="mySidenav" class="sidenav">
+              <a href="javascript:void(0)" id="closebutton" class="closebtn" >&times;</a>
+              <a href="/index.html">Home</a>
+              <a href="/pages/createnew.html">Create New</a>
+              <a href="/pages/saved.html">Saved</a>
+              <a href="/pages/search.html">Search?</a>
+              <a href="https://www.geocaching.com/play" target="_blank" rel="noopener noreferrer">Start Geocaching!</a>
+            </div>`
+    }
+    export const SendNavToDom = () => {
+        const navDom = document.querySelector(".Nav");
+        navDom.innerHTML += makeNav()
+    }
+    export function openNav(){
+        document.getElementById("NavButton").addEventListener("click", navOpen);
+    }
+    export function navOpen() {
+      document.getElementById("mySidenav").style.width = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+    export function closeNav() {
+        const closeButtonDom = document.getElementById("closebutton")
+        closeButtonDom.addEventListener("click",event => {
+            if(event.target.id === "closebutton"){
+                navClose();
+            }
+        } )
+    }
+    export function navClose() {
+        document.getElementById("mySidenav").style.width = "0px";
+    }
