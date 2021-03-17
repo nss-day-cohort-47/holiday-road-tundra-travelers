e560e10cca4dc1aa7de3374860380bd4398a8399
Carter Culkin <carter.culkin@gmail.com>
2021-03-17 13:36:01 -0500

    Tried to do the fixes Jeremiah wanted

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,10 @@ import { getEateries } from "./eateries/EateryProvider.js";
 import {sendDropDownToDom} from "./nav/dropDown.js";
 import { getParks } from "./parks/ParkProvider.js";
 import { getWeather } from "./weather/WeatherProvider.js";
-
-
+import {SendNavToDom} from "./nav/nav.js";
+import {openNav} from "./nav/nav.js";
+import {closeNav} from "./nav/nav.js";
 sendDropDownToDom()
+SendNavToDom()
+openNav()
+closeNav()
