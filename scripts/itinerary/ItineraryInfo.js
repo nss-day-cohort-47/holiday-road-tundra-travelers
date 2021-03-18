47b5664b310bfb05a3b1392cf55461bc45a270f6
Traye <johnson.traye@yahoo.com>
2021-03-18 14:06:43 -0500

    parks info shows on the dom

--- scripts/itinerary/ItineraryInfo.js
+++ scripts/itinerary/ItineraryInfo.js
@@ -2,12 +2,16 @@ import { useParksCollection, getParks } from "../parks/ParkProvider.js";
 
 const itineraryDom = document.querySelector('body')
 itineraryDom.addEventListener('change', (event) => {
-  console.log('hi', event.target.id)
   if(event.target.id === 'national-park__dropdown') {
     getParks()
     .then( () => {
       const park = useParksCollection().find(parkObj => parkObj.id === event.target.value)
-      console.log(park)
+      const addParkToDom = document.querySelector('.park')
+      addParkToDom.innerHTML = `
+      <li>Name: ${park.fullName}</li>
+      <li>Cost: $${park.entranceFees[0].cost}</li>
+      <li>Location: ${park.addresses[0].city}, ${park.addresses[0].stateCode}</li>
+      `
     })
   }
 })
